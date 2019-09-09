bc1a83a2c6fbf87347f69eefa12fd7538b96f700
housewifehacker <housewifehacker@gmail.com>
2019-09-09 07:43:03 -0500

    Add initial state

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,17 @@
 import { createStore } from 'redux';
 import rootReducer from '../reducers/reducers';
 
-export default createStore(rootReducer);
+let initialState = {
+    notes: [
+        { title: 'You are awesome', content: 'No, wait, I meant legendary!', valid: true },
+        { title: 'Ooops', content: 'I was talking to myself', valid: true }
+    ],
+    visibility: 'SHOW_VALID'
+};
+
+
+export default createStore(
+    rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
